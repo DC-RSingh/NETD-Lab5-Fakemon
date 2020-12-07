8de63591b13af537ea0f13cb329bfa88dbe3da32
Raje Singh <rajecsingh0@gmail.com>
2020-12-07 02:06:36 -0500

    add Fakemon properties

--- NETD_F2020_Lab5/Models/Fakemon.cs
+++ NETD_F2020_Lab5/Models/Fakemon.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+#nullable enable
 
 namespace NETD_F2020_Lab5.Models
 {
     public class Fakemon
     {
+        // The ID of the Fake Monster
+        public int DexNumber { get; set; }
+
+        // The Name of the Fake Monster
+        public string Name { get; set; }
+
+        // The Primary Type of the Fake Monster
+        public string TypeOne { get; set; }
+
+        // The Secondary Type of the Fake Monster
+        public string? TypeTwo { get; set; } 
+
+        // Statistical Data of the Fake Monster
+        public Stats Statistics { get; set; }
+
     }
 }
