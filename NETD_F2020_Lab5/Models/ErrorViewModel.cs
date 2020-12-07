4a71f28db11c64d1ffec99da47e5c371f66769a7
Raje Singh <rajecsingh0@gmail.com>
2020-12-06 23:22:28 -0500

    Add project files.

---
+++ NETD_F2020_Lab5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETD_F2020_Lab5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NETD_F2020_Lab5/Models/Nature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETD_F2020_Lab5.Models
+{
+    public class Nature
+    {
+    }
+}
