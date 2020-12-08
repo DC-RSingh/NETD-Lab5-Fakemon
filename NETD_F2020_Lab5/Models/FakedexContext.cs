8a6b7eeeccf14d428465337efb2851232c018e12
Raje Singh <rajecsingh0@gmail.com>
2020-12-07 21:30:47 -0500

    Simplify definition in FakedexContext

--- NETD_F2020_Lab5/Models/FakedexContext.cs
+++ NETD_F2020_Lab5/Models/FakedexContext.cs
@@ -19,7 +19,7 @@ namespace NETD_F2020_Lab5.Models
 
         public DbSet<Fakemon> Fakemons { get; set; }
 
-        public DbSet<NETD_F2020_Lab5.Models.Stats> Stats { get; set; }
+        public DbSet<Stats> Stats { get; set; }
 
         // To add database:
         // Add-Migration InitialCreate -- In Tools->Nuget Packet Manager->Packet Manager Console
