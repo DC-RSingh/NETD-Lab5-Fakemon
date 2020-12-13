20fffa80dcc8759c9b7493713e22d9f28a42e050
Raje Singh <rajecsingh0@gmail.com>
2020-12-13 17:23:39 -0500

    add User Model

---
+++ NETD_F2020_Lab5/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// Import Identity 
+using Microsoft.AspNetCore.Identity;
+
+namespace NETD_F2020_Lab5.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
