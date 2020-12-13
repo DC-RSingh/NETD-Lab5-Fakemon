cf703c4e989f719c140bcebef28a4e3697d1842c
Raje Singh <rajecsingh0@gmail.com>
2020-12-13 17:53:55 -0500

    update Migrations

--- NETD_F2020_Lab5/Migrations/20201213225149_InitialCreate.Designer.cs
+++ NETD_F2020_Lab5/Migrations/20201213225149_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NETD_F2020_Lab5.Models;
 
 namespace NETD_F2020_Lab5.Migrations
 {
     [DbContext(typeof(FakedexContext))]
-    partial class FakedexContextModelSnapshot : ModelSnapshot
+    [Migration("20201213225149_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
