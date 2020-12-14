4487da7123a109096f0dd12be9785f2be7d75c99
Raje Singh <rajecsingh0@gmail.com>
2020-12-13 19:18:34 -0500

    add scaffolded pages for Identity Area

---
+++ NETD_F2020_Lab5/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NETD_F2020_Lab5.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NETD_F2020_Lab5/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using NETD_F2020_Lab5.Models;
+
+namespace NETD_F2020_Lab5.Areas.Identity.Pages.Account.Manage
+{
+    public class ShowRecoveryCodesModel : PageModel
+    {
+        [TempData]
+        public string[] RecoveryCodes { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (RecoveryCodes == null || RecoveryCodes.Length == 0)
+            {
+                return RedirectToPage("./TwoFactorAuthentication");
+            }
+
+            return Page();
+        }
+    }
+}
