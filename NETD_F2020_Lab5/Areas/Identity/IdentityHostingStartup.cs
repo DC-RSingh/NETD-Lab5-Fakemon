8c1bf304e713ebdc59c39769a5dc631bbbb7f3b6
Raje Singh <rajecsingh0@gmail.com>
2020-12-13 19:18:34 -0500

    add scaffolded pages for Identity Area

---
+++ NETD_F2020_Lab5/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NETD_F2020_Lab5.Models;
+
+[assembly: HostingStartup(typeof(NETD_F2020_Lab5.Areas.Identity.IdentityHostingStartup))]
+namespace NETD_F2020_Lab5.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ NETD_F2020_Lab5/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NETD_F2020_Lab5.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
