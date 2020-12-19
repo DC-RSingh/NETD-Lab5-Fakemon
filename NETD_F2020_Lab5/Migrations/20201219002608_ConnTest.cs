fd8caaa0943f9e6d4081ebf26001eb5d963f2097
Raje Singh <raje.singh@dcmail.ca>
2020-12-18 20:23:24 -0500

    connection test migrations

---
+++ NETD_F2020_Lab5/Migrations/20201219002608_ConnTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NETD_F2020_Lab5.Migrations
+{
+    public partial class ConnTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
